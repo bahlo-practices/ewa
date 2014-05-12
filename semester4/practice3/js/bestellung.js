9ca8eb5bab574f5a0081fdad155aa8152309b365
Arne Bahlo <hallo@arne.me>
2014-05-12 14:31:36 +0200

    Define variables at top

--- semester4/practice3/js/bestellung.js
+++ semester4/practice3/js/bestellung.js
@@ -2,28 +2,35 @@
 
 // Wait until DOM content is ready
 window.addEventListener('load', function() {
-  var cart = document.querySelector('#orders'),
-      priceTag = document.querySelector('#sum'),
-      sum = 0;
+  var cart, priceTag, sum, addPizza, id, name, price, option, pizzaElements, i;
+
+  //
+  // Add pizzas
+  //
+  cart = document.querySelector('#orders');
+  priceTag = document.querySelector('#sum');
+  sum = 0;
 
   // Function to add pizzas to the cart
-  var addPizza = function() {
+  addPizza = function() {
     // Get attributes from HTML
-    var id = this.getAttribute('data-id'),
-        name = this.getAttribute('data-name'),
-        price = parseInt(this.getAttribute('data-price'), 10) / 100;
+    id = this.getAttribute('data-id');
+    name = this.getAttribute('data-name');
+    price = parseInt(this.getAttribute('data-price'), 10);
 
     // Add to cart
-    cart[cart.length] = new Option(name, id);
+    option = new Option(name, id);
+    option.setAttribute('data-price', price);
+    cart[cart.length] = option;
 
     // Add new price
-    sum += price;
+    sum += price / 100;
     priceTag.innerHTML = sum.toFixed(2);
   };
 
   // Get all pizza-elements and add click event listener for each
-  var pizzaElements = document.querySelectorAll('.add-pizza');
-  for (var i = 0;i < pizzaElements.length;++i) {
+  pizzaElements = document.querySelectorAll('.add-pizza');
+  for (i = 0;i < pizzaElements.length;++i) {
     pizzaElements[i].addEventListener('click', addPizza);
   }
 });
