b720bd96125334b07d771f9de8deb98be1dd6817
Arne Bahlo <hallo@arne.me>
2014-05-12 13:58:58 +0200

    Cleaner code

---
+++ semester4/practice3/js/bestellung.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Wait until DOM content is ready
+window.addEventListener('load', function() {
+  // Function to add pizzas to the cart
+  var addPizza = function() {
+    var id = this.getAttribute('data-id'),
+        name = this.getAttribute('data-name'),
+        price = this.getAttribute('data-price');
+
+    console.log(id, name, price);
+  };
+
+  // Get all pizza-elements and add click event listener for each
+  var pizzaElements = document.querySelectorAll('.add-pizza');
+  for (var i = 0;i < pizzaElements.length;++i) {
+    pizzaElements[i].addEventListener('click', addPizza);
+  }
+});
