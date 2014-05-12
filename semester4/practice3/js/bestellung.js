1850ddde99345eaae8dd94997511daeac63cccdc
Arne Bahlo <hallo@arne.me>
2014-05-12 14:06:35 +0200

    Finish add-to-cart

--- semester4/practice3/js/bestellung.js
+++ semester4/practice3/js/bestellung.js
@@ -2,6 +2,8 @@
 
 // Wait until DOM content is ready
 window.addEventListener('load', function() {
+  var cart = document.querySelector('#orders');
+
   // Function to add pizzas to the cart
   var addPizza = function() {
     var id = this.getAttribute('data-id'),
@@ -9,6 +11,7 @@ window.addEventListener('load', function() {
         price = this.getAttribute('data-price');
 
     console.log(id, name, price);
+    cart[cart.length] = new Option(name, id);
   };
 
   // Get all pizza-elements and add click event listener for each
