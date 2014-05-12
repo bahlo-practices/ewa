b386d7505180e04068ba2ff670bf9ff67f0766d7
Arne Bahlo <hallo@arne.me>
2014-05-12 14:40:02 +0200

    Implement reset of pizzas

--- semester4/practice3/js/bestellung.js
+++ semester4/practice3/js/bestellung.js
@@ -2,16 +2,26 @@
 
 // Wait until DOM content is ready
 window.addEventListener('load', function() {
-  var cart, priceTag, sum, addPizza, id, name, price, option, pizzaElements, i;
+  var cart, priceTag, sum, updateSum, addPizza, id, name, price, option,
+      pizzaElements, i, resetPizzas;
 
-  //
-  // Add pizzas
-  //
+  // Define variables
   cart = document.querySelector('#orders');
   priceTag = document.querySelector('#sum');
   sum = 0;
 
-  // Function to add pizzas to the cart
+  /**
+   * Updates the sum and it's element with the new value
+   * @param  {integer} newSum The new sum
+   */
+  updateSum = function(newSum) {
+    sum = newSum;
+    priceTag.innerHTML = sum.toFixed(2);
+  }
+
+  /**
+   * Adds pizzas to the cart
+   */
   addPizza = function() {
     // Get attributes from HTML
     id = this.getAttribute('data-id');
@@ -24,13 +34,25 @@ window.addEventListener('load', function() {
     cart[cart.length] = option;
 
     // Add new price
-    sum += price / 100;
-    priceTag.innerHTML = sum.toFixed(2);
+    updateSum(sum + price / 100);
   };
 
   // Get all pizza-elements and add click event listener for each
-  pizzaElements = document.querySelectorAll('.add-pizza');
-  for (i = 0;i < pizzaElements.length;++i) {
+  for (i = 0,
+       pizzaElements = document.querySelectorAll('.add-pizza');
+       i < pizzaElements.length;
+       ++i) {
     pizzaElements[i].addEventListener('click', addPizza);
   }
+
+  /**
+   * Resets the selected pizzas
+   */
+  resetPizzas = function() {
+    cart.innerHTML = '';
+    updateSum(0);
+  }
+
+  // Reset button
+  document.querySelector('#delete-all').addEventListener('click', resetPizzas);
 });
