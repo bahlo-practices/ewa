dd35635d3f8fc10ca5d729077d65b2c64531d241
Arne Bahlo <hallo@arne.me>
2014-05-26 16:18:44 +0200

    Replace <CR> with <LS>

--- semester4/practice5/BlockTemplate.php
+++ semester4/practice5/BlockTemplate.php
@@ -4,29 +4,29 @@
  * Demonstrates use of PHP including class and OO.
  * Implements Zend coding standards.
  * Generate documentation with Doxygen or phpdoc
- * 
+ *
  * PHP Version 5
  *
  * @category File
  * @package  Pizzaservice
- * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de> 
- * @author   Ralf Hahn, <ralf.hahn@h-da.de> 
- * @license  http://www.h-da.de  none 
+ * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de>
+ * @author   Ralf Hahn, <ralf.hahn@h-da.de>
+ * @license  http://www.h-da.de  none
  * @Release  1.2
- * @link     http://www.fbi.h-da.de 
+ * @link     http://www.fbi.h-da.de
  */
 
 /**
- * This is a template for classes, which represent div-blocks 
- * within a web page. Instances of these classes are used as members 
+ * This is a template for classes, which represent div-blocks
+ * within a web page. Instances of these classes are used as members
  * of top level classes.
- * The order of methods might correspond to the order of thinking 
+ * The order of methods might correspond to the order of thinking
  * during implementation.
- 
- * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de> 
- * @author   Ralf Hahn, <ralf.hahn@h-da.de> 
+
+ * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de>
+ * @author   Ralf Hahn, <ralf.hahn@h-da.de>
 */
- 
+
 class BlockTemplate        // to do: change name of class
 {
     // --- ATTRIBUTES ---
@@ -36,21 +36,21 @@ class BlockTemplate        // to do: change name of class
      * accessed by all operations of the class.
      */
     protected $_database = null;
-    
-    // to do: declare reference variables for members 
+
+    // to do: declare reference variables for members
     // representing substructures/blocks
-    
+
     // --- OPERATIONS ---
-    
+
     /**
      * Gets the reference to the DB from the calling page template.
      * Stores the connection in member $_database.
      *
-     * @param $database $database is the reference to the DB to be used     
+     * @param $database $database is the reference to the DB to be used
      *
      * @return none
      */
-    public function __construct($database) 
+    public function __construct($database)
     {
         $this->_database = $database;
         // to do: instantiate members representing substructures/blocks
@@ -66,17 +66,17 @@ class BlockTemplate        // to do: change name of class
     {
         // to do: fetch data for this view from the database
     }
-    
+
     /**
      * Generates an HTML block embraced by a div-tag with the submitted id.
-     * If the block contains other blocks, delegate the generation of their 
+     * If the block contains other blocks, delegate the generation of their
 	 * parts of the view to them.
      *
-     * @param $id $id is the unique (!!) id to be used as id in the div-tag     
+     * @param $id $id is the unique (!!) id to be used as id in the div-tag
      *
      * @return none
      */
-    public function generateView($id = "") 
+    public function generateView($id = "")
     {
         $this->getViewData();
         if ($id) {
@@ -86,15 +86,15 @@ class BlockTemplate        // to do: change name of class
         // to do: call generateView() for all members
         echo "</div>\n";
     }
-    
+
     /**
      * Processes the data that comes via GET or POST i.e. CGI.
      * If this block is supposed to do something with submitted
-     * data do it here. 
-     * If the block contains other blocks, delegate processing of the 
+     * data do it here.
+     * If the block contains other blocks, delegate processing of the
 	 * respective subsets of data to them.
      *
-     * @return none 
+     * @return none
      */
     public function processReceivedData()
     {
@@ -102,8 +102,8 @@ class BlockTemplate        // to do: change name of class
     }
 }
 // Zend standard does not like closing php-tag!
-// PHP doesn't require the closing tag (it is assumed when the file ends). 
-// Not specifying the closing ? >  helps to prevent accidents 
-// like additional whitespace which will cause session 
-// initialization to fail ("headers already sent"). 
+// PHP doesn't require the closing tag (it is assumed when the file ends).
+// Not specifying the closing ? >  helps to prevent accidents
+// like additional whitespace which will cause session
+// initialization to fail ("headers already sent").
 //? >
